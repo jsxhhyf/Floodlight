3ff3543cb6120d79d3d52a219e71502abc3443a8
jsxhhyf <jsxhhyf@gmail.com>
2015-01-14 22:24:13 +0800

    change the flow add pattern to proactively instead of adding flow entries after accepting PacketIn

--- src/main/java/net/floodlightcontroller/flowadd/Flowadd.java
+++ src/main/java/net/floodlightcontroller/flowadd/Flowadd.java
@@ -20,6 +20,7 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 import org.projectfloodlight.openflow.protocol.OFFactory;
 import org.projectfloodlight.openflow.protocol.OFFlowAdd;
@@ -35,10 +36,12 @@ import org.projectfloodlight.openflow.protocol.instruction.OFInstructionApplyAct
 import org.projectfloodlight.openflow.protocol.instruction.OFInstructions;
 import org.projectfloodlight.openflow.protocol.match.Match;
 import org.projectfloodlight.openflow.protocol.match.MatchField;
+import org.projectfloodlight.openflow.types.DatapathId;
 import org.projectfloodlight.openflow.types.MacAddress;
 import org.projectfloodlight.openflow.types.OFBufferId;
 import org.projectfloodlight.openflow.types.OFPort;
 import org.python.antlr.PythonParser.return_stmt_return;
+import org.python.modules.thread.thread;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -52,6 +55,7 @@ import net.floodlightcontroller.core.module.FloodlightModuleException;
 import net.floodlightcontroller.core.module.IFloodlightModule;
 import net.floodlightcontroller.core.module.IFloodlightService;
 import net.floodlightcontroller.core.util.AppCookie;
+import net.floodlightcontroller.packet.Data;
 
 /**
  * @author phillip
@@ -67,6 +71,8 @@ public class Flowadd implements IFloodlightModule, IOFMessageListener {
 	protected IFloodlightProviderService floodlightProvider;
 
 	private Match match;
+	private Set<DatapathId> switches;
+	private OFFactory ofFactory;
 
 	/*
 	 * (non-Javadoc)
@@ -118,7 +124,8 @@ public class Flowadd implements IFloodlightModule, IOFMessageListener {
 	public void init(FloodlightModuleContext context)
 			throws FloodlightModuleException {
 		switchService = context.getServiceImpl(IOFSwitchService.class);
-		floodlightProvider = context.getServiceImpl(IFloodlightProviderService.class);
+		floodlightProvider = context
+				.getServiceImpl(IFloodlightProviderService.class);
 	}
 
 	/*
@@ -131,6 +138,36 @@ public class Flowadd implements IFloodlightModule, IOFMessageListener {
 	public void startUp(FloodlightModuleContext context)
 			throws FloodlightModuleException {
 		floodlightProvider.addOFMessageListener(OFType.PACKET_IN, this);
+		do {
+			try {
+				Thread.sleep(1000);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		} while (switchService.getAllSwitchDpids().size() < 4);
+		for (int i = 1; i < 4; i = i + 2) { // switch1 & switch3
+			ofFactory = switchService.getSwitch(DatapathId.of(i))
+					.getOFFactory();
+			for (int j = 1; j <= 6; j++) {
+				match = ofFactory.buildMatch()
+						.setExact(MatchField.IN_PORT, OFPort.of(j)).build();
+				addFlowMod(switchService.getSwitch(DatapathId.of(i)), match,
+						j < 4 ? (short) (j + 3) : (short) (j - 3));
+			}
+		}
+		for (int i = 2; i <= 4; i = i + 2) { // switch2 & switch4
+			ofFactory = switchService.getSwitch(DatapathId.of(i))
+					.getOFFactory();
+			match = ofFactory.buildMatch()
+					.setExact(MatchField.IN_PORT, OFPort.of(1)).build();
+			addFlowMod(switchService.getSwitch(DatapathId.of(i)), match,
+					(short)2);
+			match = ofFactory.buildMatch()
+					.setExact(MatchField.IN_PORT, OFPort.of(2)).build();
+			addFlowMod(switchService.getSwitch(DatapathId.of(i)), match,
+					(short)1);
+		}
 	}
 
 	/*
@@ -182,68 +219,17 @@ public class Flowadd implements IFloodlightModule, IOFMessageListener {
 	@Override
 	public net.floodlightcontroller.core.IListener.Command receive(
 			IOFSwitch sw, OFMessage msg, FloodlightContext cntx) {
-		switch (msg.getType()) {
-		case PACKET_IN:
-			log.info("PACKET_IN packet received from sw" + sw.getId());
-			OFFactory ofFactory = sw.getOFFactory();
-			OFPacketIn pi = (OFPacketIn) msg;
-			// only no match flow entry leads to add new flow entry
-			if (!pi.getReason().equals(OFPacketInReason.NO_MATCH)) {
-				log.info("not NO_MATCH and ignore it");
-				return Command.CONTINUE;
-			}
-			match = pi.getMatch().createBuilder().build();
-			switch ((int) sw.getId().getLong()) {
-			case 1:
-				if (pi.getInPort().getPortNumber() < 4) {
-					addFlowMod(sw, match,
-							(short) (pi.getInPort().getPortNumber() + 3));
-				} else {
-					addFlowMod(sw, match,
-							(short) (pi.getInPort().getPortNumber() - 3));
-				}
-				break;
-			case 2:
-				if (pi.getInPort().getPortNumber() == 1) {
-					addFlowMod(sw, match, (short) 2);
-				} else {
-					addFlowMod(sw, match, (short) 1);
-				}
-				break;
-			case 3:
-				if (pi.getInPort().getPortNumber() < 4) {
-					addFlowMod(sw, match,
-							(short) (pi.getInPort().getPortNumber() + 3));
-				} else {
-					addFlowMod(sw, match,
-							(short) (pi.getInPort().getPortNumber() - 3));
-				}
-				break;
-			case 4:
-				if (pi.getInPort().getPortNumber() == 1) {
-					addFlowMod(sw, match, (short) 2);
-				} else {
-					addFlowMod(sw, match, (short) 1);
-				}
-				break;
-			default:
-				break;
-			}
 			return Command.CONTINUE;
-		default:
-			log.info("1111");
-			return Command.CONTINUE;
-		}
 	}
 
 	public void addFlowMod(IOFSwitch sw, Match match, short outport) {
-		if (match.get(MatchField.ETH_DST).equals(
-				MacAddress.of("01:80:c2:00:00:0e"))
-				|| match.get(MatchField.ETH_DST).equals(
-						MacAddress.of("ff:ff:ff:ff:ff:ff"))) {
-			// log.info("LLDP or BBDP");//by phil 2014-10-31
-			return;
-		}
+//		if (match.get(MatchField.ETH_DST).equals(
+//				MacAddress.of("01:80:c2:00:00:0e"))
+//				|| match.get(MatchField.ETH_DST).equals(
+//						MacAddress.of("ff:ff:ff:ff:ff:ff"))) {
+//			// log.info("LLDP or BBDP");//by phil 2014-10-31
+//			return;
+//		}
 		// byte[] b = match.getDataLayerDestination();
 		// for (int i = 0; i < b.length; i++) {
 		// String hex = Integer.toHexString(b[i] & 0xFF);
